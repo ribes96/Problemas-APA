04b24bb85b263af221d60702214f39bf52e87ad5
ribes96 <aribes96@gmail.com>
2017-11-12 13:47:22 +0100

    Terminado el problema 4

--- Lista3/P4/sc.R
+++ Lista3/P4/sc.R
@@ -1,14 +1,17 @@
-f = c(1,2,3,4,5)
-l = c(7.97, 10.2, 14.2, 16.0, 21.2)
+f = matrix(1:5, ncol = 1)
+l = matrix(c(7.97, 10.2, 14.2, 16.0, 21.2), ncol = 1)
 
-df = data.frame(f, l)
+#df = data.frame(f, l)
 
-model = lm(formula = l ~  f,  data = df)
+#model = lm(formula = l ~  f,  data = df)
 
-phi.0 = c(rep(model$coefficients[1],5))
-phi.1 = model$coefficients[2] * f
+#phi.0 = c(rep(model$coefficients[1],5))
+#phi.1 = model$coefficients[2] * f
 
-PHI = matrix(c(phi.0, phi.1), ncol = 2)
+phi.0 = function(x) return(1)
+phi.1 = function(x) return(x)
+
+PHI = cbind(apply(f, MARGIN = 1, FUN = phi.0), apply(f, MARGIN = 1, FUN = phi.1))
 
 library(MASS)
 
